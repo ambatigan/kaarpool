bf08e863a8e81030334ea404e694bc8639790822
vroshan <vroshan@329227c8-4d19-4762-87ff-c139c1e8da10>
2011-03-08 11:41:50 +0000

    Created basic Kapool client application
    git-svn-id: http://198.162.18.10:9090/svn/karpool/trunk@9 329227c8-4d19-4762-87ff-c139c1e8da10

---
+++ client/Karpool/src/com/saventech/karpool/Karpool.java
@@ -0,0 +1,13 @@
+package com.saventech.karpool;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Karpool extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
