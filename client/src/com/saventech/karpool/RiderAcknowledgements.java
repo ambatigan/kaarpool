3ec48e86a70329e53a608c3ffb0d839c150df9aa
lakshmikantha <lakshmikantha@329227c8-4d19-4762-87ff-c139c1e8da10>
2011-03-25 10:24:27 +0000

    Written code in RiderAcknowledgements.java to implement status bar notification feature
    git-svn-id: http://198.162.18.10:9090/svn/karpool/trunk@37 329227c8-4d19-4762-87ff-c139c1e8da10

--- client/src/com/saventech/karpool/RiderAcknowledgements.java
+++ client/src/com/saventech/karpool/RiderAcknowledgements.java
@@ -1,6 +1,12 @@
 package com.saventech.karpool;
 
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +28,8 @@ public class RiderAcknowledgements extends Activity implements OnClickListener {
         setContentView(R.layout.rider_acknowledgements);
         Button bb =(Button)findViewById(R.id.OkButton);
         bb.setOnClickListener(this);
+        Button bb1 = (Button)findViewById(R.id.notificationbutton);
+        bb1.setOnClickListener(this);
     }
     
     private class OnReadyListener implements RatingDialog.ReadyListener {
@@ -36,8 +44,43 @@ public class RiderAcknowledgements extends Activity implements OnClickListener {
 	public void onClick(View v) {
 		// TODO Auto-generated method stub
 		
-		RatingDialog myDialog = new RatingDialog(this.getParent(), "", new OnReadyListener());
-	    myDialog.show();
+		if(v==findViewById(R.id.OkButton))
+		{
+			RatingDialog myDialog = new RatingDialog(this.getParent(), "", new OnReadyListener());
+		    myDialog.show();
+		}
+		if(v==findViewById(R.id.notificationbutton))
+		{
+			NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+	    	
+	    	int icon = R.drawable.ic_tab_artists_white;
+	    	CharSequence text = "pick up request";
+	    	CharSequence contentTitle = "Ride pickup request from Roshan";
+	    	CharSequence contentText = "Request has been accepted. Please confirm pickup";
+	    	long when = System.currentTimeMillis();
+	    	
+	    	//RiderJourneyDetails ParentActivity = (RiderJourneyDetails) this.getParent();
+            //ParentActivity.switchTab(2);
+	    	Intent intent = new Intent(this, JourneyDetails.class);
+	    	PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
+	    	Notification notification = new Notification(icon,text,when);
+	    	
+	    	long[] vibrate = {0,100,200,300};
+	    	notification.vibrate = vibrate;
+	    	
+	    	notification.ledARGB = Color.RED;
+	    	notification.ledOffMS = 300;
+	    	notification.ledOnMS = 300;
+	    	
+	    	notification.defaults |= Notification.DEFAULT_LIGHTS;
+	    	//notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+	    	
+	    	notification.setLatestEventInfo(this, contentTitle, contentText, contentIntent);
+	    	
+	    	notificationManager.notify(1001, notification);
+	    	RiderJourneyDetails ParentActivity = (RiderJourneyDetails) this.getParent();
+            ParentActivity.switchTab(2);
+		}
 		
 	}
 }
