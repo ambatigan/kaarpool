8aa1806db4019eabcf710e7ab381f9b9c6ab756a
lakshmikantha <lakshmikantha@329227c8-4d19-4762-87ff-c139c1e8da10>
2011-03-25 07:02:42 +0000

    Added comments and log statements
    git-svn-id: http://198.162.18.10:9090/svn/karpool/trunk@32 329227c8-4d19-4762-87ff-c139c1e8da10

---
+++ client/src/com/saventech/karpool/CancelRoute.java
@@ -0,0 +1,66 @@
+/**
+ * Project: Karpool
+ * Package: com.saventech.karpool
+ * File: Cancelroute.java
+ * Date: Mar 25, 2011
+ * It is responsible to cancel current ride for driver
+ */
+
+package com.saventech.karpool;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class CancelRoute extends Activity implements OnClickListener 
+{
+	
+	private EditText source;
+	@SuppressWarnings("unused")
+	private EditText destination;
+	@SuppressWarnings("unused")
+	private EditText starttime;
+	private Button cancelroutebutton;
+	private boolean cancelrouteflag;
+	Controller controller;
+	
+    public void onCreate(Bundle savedInstanceState) 
+    {
+    	
+        super.onCreate(savedInstanceState);
+        Log.i("DriverJourneyDetails_Cancel route", "Cancel route tab in DriverJourneyDetails");
+        controller=new Controller();
+        setContentView(R.layout.cancelroute);
+        source=(EditText)findViewById(R.id.drivercancelroutesource);
+        destination=(EditText)findViewById(R.id.drivercancelroutedestination);
+        starttime=(EditText)findViewById(R.id.drivercancelroutestarttime);
+        
+        cancelroutebutton=(Button)findViewById(R.id.drivercancelroute);
+        cancelroutebutton.setOnClickListener(this);
+        
+    }
+
+	public void onClick(View v) 
+	{
+		Log.i("Cancelroute_onClick", "Cancelroute button pressed");
+		switch(v.getId())
+		{
+			case R.id.drivercancelroute:
+				cancelrouteflag=controller.Canceldriverroute();
+				if(cancelrouteflag)
+				{
+					Toast.makeText(this, "Route has been canceled", Toast.LENGTH_LONG).show();
+					source.clearFocus();
+				}
+				break;
+			
+		}
+		
+	}
+    
+}
