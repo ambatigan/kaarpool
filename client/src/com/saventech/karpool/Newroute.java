3611bf70888db31b5cc47e2366b829b25efd19f4
lakshmikantha <lakshmikantha@329227c8-4d19-4762-87ff-c139c1e8da10>
2011-04-27 13:00:53 +0000

    added code to subscribe meteor with driver name as a channel using deacon libray
    git-svn-id: http://198.162.18.10:9090/svn/karpool/trunk@132 329227c8-4d19-4762-87ff-c139c1e8da10

--- client/src/com/saventech/karpool/Newroute.java
+++ client/src/com/saventech/karpool/Newroute.java
@@ -7,14 +7,25 @@
 
 package com.saventech.karpool;
 
+import java.io.PrintWriter;
 import java.util.Calendar;
+
+import org.deacon.Deacon;
+import org.deacon.DeaconError;
+import org.deacon.DeaconObserver;
+import org.deacon.DeaconResponse;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.graphics.Color;
 import android.os.Bundle;
 
 import android.util.Log;
@@ -29,7 +40,7 @@ import android.widget.Toast;
 
 import com.saventech.karpool.DateTimePicker;
 
-public class Newroute extends Activity implements OnClickListener{
+public class Newroute extends Activity implements OnClickListener, DeaconObserver{
 	
 	private String value="";
 	private String datetime="";
@@ -45,7 +56,9 @@ public class Newroute extends Activity implements OnClickListener{
 	private SharedPreferences mPreferences; 
 	Session session;
 	Validations validate;
-
+	
+	private Deacon deacon;
+	PrintWriter outToServer;
 	
     public void onCreate(Bundle savedInstanceState) {
     	
@@ -90,6 +103,19 @@ public class Newroute extends Activity implements OnClickListener{
         	
 			
 		}*/
+        
+        //Meteor connectivity
+        try 
+        {
+        	this.deacon = new Deacon("198.162.18.171",4670, this);
+        	deacon.catchUpTimeOut(60);
+        	deacon.register(this);
+        } 
+        catch (Exception e) 
+        {
+        	System.out.println("Problem while creating Deacon");
+        	e.printStackTrace();
+        }
     }
     
     public boolean onKeyDown(int keyCode, KeyEvent event) 
@@ -183,7 +209,6 @@ public class Newroute extends Activity implements OnClickListener{
 		((Button) mDateTimeDialogView.findViewById(R.id.CancelDialog)).setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				mDateTimeDialog.cancel();
 			}
 		});
@@ -192,7 +217,6 @@ public class Newroute extends Activity implements OnClickListener{
 		((Button) mDateTimeDialogView.findViewById(R.id.ResetDateTime)).setOnClickListener(new OnClickListener() {
 
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				mDateTimePicker.reset();
 			}
 		});
@@ -239,6 +263,8 @@ public class Newroute extends Activity implements OnClickListener{
 						DriverJourneyDetails ParentActivity = (DriverJourneyDetails) this.getParent();
 			            ParentActivity.switchTab(2);
 					}
+					System.out.println("vvvvvvvvusername: "+ session.getUsername(mPreferences));
+					deacon.joinChannel(session.getUsername(mPreferences), 0);
 				}
 				
 			}
@@ -246,6 +272,14 @@ public class Newroute extends Activity implements OnClickListener{
 			{
 				Toast.makeText(Newroute.this, "Please make sure all fields are filled", Toast.LENGTH_LONG).show();
 			}
+			try {
+				deacon.start();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			//outToServer.println("ADDMESSAGE "+session.getUsername(mPreferences)+" "+str1);
+			Log.i("Newroute_onClick", "Meteor subscriber channel created with username");
 			
 			
 		}
@@ -303,6 +337,60 @@ public class Newroute extends Activity implements OnClickListener{
         	System.out.println("if condition in driverjourneysettime");
         	showDateTimeDialog();
         }
+		
+	}
+
+	public void onError(DeaconError arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void onPush(DeaconResponse response) {
+		// TODO Auto-generated method stub
+		System.out.println("payload from meteor: "+ response.getPayload());
+		notificationAlarm();
+	}
+
+	private void notificationAlarm() {
+		// TODO Auto-generated method stub
+		NotificationManager notificationManager = (NotificationManager)getSystemService(Context.NOTIFICATION_SERVICE);
+    	
+    	int icon = R.drawable.ic_tab_artists_white;
+    	CharSequence text = "pick up request";
+    	CharSequence contentTitle = "Ride pickup request from Roshan";
+    	CharSequence contentText = "Request has been accepted. Please confirm pickup";
+    	long when = System.currentTimeMillis();
+    	
+    	//RiderJourneyDetails ParentActivity = (RiderJourneyDetails) this.getParent();
+        //ParentActivity.switchTab(2);
+    	Intent intent = new Intent(this, JourneyDetails.class);
+    	PendingIntent contentIntent = PendingIntent.getActivity(this, 0, intent, 0);
+    	Notification notification = new Notification(icon,text,when);
+    	
+    	//notification.defaults |= Notification.FLAG_AUTO_CANCEL;
+    	//notification.defaults |= Notification.DEFAULT_SOUND;
+    	notification.flags = Notification.DEFAULT_SOUND | Notification.FLAG_AUTO_CANCEL;
+    	
+    	long[] vibrate = {0,100,200,300};
+    	notification.vibrate = vibrate;
+    	
+    	notification.ledARGB = Color.RED;
+    	notification.ledOffMS = 300;
+    	notification.ledOnMS = 300;
+    	
+    	notification.defaults |= Notification.DEFAULT_LIGHTS;
+    	//notification.flags |= Notification.FLAG_SHOW_LIGHTS;
+    	
+    	notification.setLatestEventInfo(this, contentTitle, contentText, contentIntent);
+    	
+    	notificationManager.notify(1001, notification);
+    	//RiderJourneyDetails ParentActivity = (RiderJourneyDetails)this.getParent();
+        //ParentActivity.switchTab(2);
+		
+	}
+
+	public void onReconnect() {
+		// TODO Auto-generated method stub
 		
 	}
 }
