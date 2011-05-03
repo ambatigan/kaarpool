a6dcdd49e6c5707b8f129049838fe75ee27b4ee5
lakshmikantha <lakshmikantha@329227c8-4d19-4762-87ff-c139c1e8da10>
2011-05-03 11:30:03 +0000

    Added code to handle exception
    git-svn-id: http://198.162.18.10:9090/svn/karpool/trunk@168 329227c8-4d19-4762-87ff-c139c1e8da10

--- server/src/com/saventech/kaarpool/InjecttoMeteor.java
+++ server/src/com/saventech/kaarpool/InjecttoMeteor.java
@@ -14,22 +14,25 @@ public class InjecttoMeteor
 	public String injecttoMeteor(String events,String Meteorip,String Meteorport) throws IOException
 	{
 		String eventlist[]=events.toString().trim().split("EVENT");
+		try{
 		Socket clientSocket=new Socket(Meteorip,Integer.parseInt(Meteorport));
 		PrintWriter outToServer = new PrintWriter(clientSocket.getOutputStream(),true);
 		System.out.println("connected to meteor server");
-		try{
+		
 			for(int i=0;i<eventlist.length;i++)
 			{
 			    outToServer.println(eventlist[i].toString().trim());
 			}
+			clientSocket.close();
+			outToServer.close();
 			
 		}catch(Exception e)
 		{
 			e.printStackTrace();
 			log.info("Exception occur while injecting events");
+			return "Exception occur while injecting evetns";
 		}
-		clientSocket.close();
-		outToServer.close();
+		
 		return "successfully injected";
 	}
 	
