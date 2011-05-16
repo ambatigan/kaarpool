6469aa2d96fb33d6ec5b2b4ec1e22902179bca5e
lakshmikantha <lakshmikantha@329227c8-4d19-4762-87ff-c139c1e8da10>
2011-05-16 12:46:42 +0000

    added gps functionality to kaarpool, as of now it shows current location address, map and time & distance of a current location to given location
    git-svn-id: http://198.162.18.10:9090/svn/karpool/trunk@194 329227c8-4d19-4762-87ff-c139c1e8da10

--- client/src/com/saventech/karpool/TrackRoute.java
+++ client/src/com/saventech/karpool/TrackRoute.java
@@ -1,15 +1,59 @@
 package com.saventech.karpool;
 
-import android.app.Activity;
-import android.content.SharedPreferences;
-import android.os.Bundle;
-import android.view.Window;
-import android.widget.TextView;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.Locale;
 
-public class TrackRoute extends Activity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Window;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapController;
+import com.google.android.maps.MapView;
+
+public class TrackRoute extends MapActivity {
+	
+	private MapController mapController;
+	private MapView mapView;
+	private LocationManager locationManager;
+	private TextView address;
+	private TextView distance;
 	private SharedPreferences mPreferences; 
+	
+	private double fromLat;
+	private double fromLon;
+	private double toLat;
+	private double toLon;
+	private String timeToDestination;
+	private String distanceToDestination;
+	
 	 Session session;
-    @Override
+    @SuppressWarnings("unused")
+	@Override
     protected void onCreate(Bundle savedInstanceState) 
     {
     super.onCreate(savedInstanceState);
@@ -29,8 +73,171 @@ public class TrackRoute extends Activity {
     String name[]= username.split("@");
     rightText.setText(name[0]);
    
-    
-    
-    
+    address = (TextView)findViewById(R.id.trackrouteadd);
+	distance = (TextView)findViewById(R.id.trackroutedst);
+	// create a map view
+	LinearLayout linearLayout = (LinearLayout) findViewById(R.id.mainlayout);
+	mapView = (MapView) findViewById(R.id.mapview);
+	mapView.setBuiltInZoomControls(true);
+	mapView.setStreetView(true);
+	mapController = mapView.getController();
+	mapController.setZoom(14); // Zoon 1 is world view
+	locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+	locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0,
+	        0, new GeoUpdateHandler());
    }
+	@Override
+	protected boolean isRouteDisplayed() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+	
+	public class GeoUpdateHandler implements LocationListener {
+
+		public void onLocationChanged(Location location) {
+			int lat = (int) (location.getLatitude() * 1E6);
+			int lng = (int) (location.getLongitude() * 1E6);
+			double lat1 = location.getLatitude();
+			double lng1 = location.getLongitude();
+			double lat2 = 17.445872;
+			double lng2 = 78.352625;
+			String add1 = lat1+","+lng1;
+			String add2 = lat2+","+lng2;
+			getDistancenTime(getUrl(add1,add2));
+			Location location1 = new Location("reverseGeocoded");
+			location1.setLatitude(lat2);
+			location1.setLongitude(lng2);
+			//System.out.println("distance: "+location.distanceTo(location1));
+			//String str = String.valueOf((location.distanceTo(location1))/1000) + " kms";
+			//distance.setText(str);
+			Geocoder geocoder=new Geocoder(getBaseContext(),Locale.getDefault()); 
+			try 
+			{ 
+				String city=""; 
+				List<Address> addresses= geocoder.getFromLocation(lat1, lng1, 1); 
+				if(addresses.size()>0) 
+				{ 
+					//city+=addresses.get(0).getSubAdminArea(); 
+					Address address = addresses.get(0);
+	                
+	                System.out.println("address if condition      ");
+	                for (int i = 0; i < address.getMaxAddressLineIndex(); i++)
+	                {
+	                	System.out.println(address.getAddressLine(i));
+	                    city+=address.getAddressLine(i)+" ";
+	                }
+
+	                //city+=address.getLocality()+" ";
+	                //city+=address.getPostalCode()+" ";
+	                city+=address.getCountryName();
+				}
+				//System.out.println("city name: "+city);
+				if(city=="")
+					address.setText("No address found");
+				else
+					address.setText(city);
+				Toast.makeText(getBaseContext(),"Address: "+city,Toast.LENGTH_SHORT).show();
+			}
+			catch (IOException e) 
+			{ 
+				e.printStackTrace(); 
+			}
+			
+			GeoPoint point = new GeoPoint(lat, lng);
+			mapController.animateTo(point); //	mapController.setCenter(point);
+		}
+
+		public void onProviderDisabled(String provider) {
+			// TODO Auto-generated method stub
+		}
+
+		public void onProviderEnabled(String provider) {
+			// TODO Auto-generated method stub
+			
+		}
+
+		public void onStatusChanged(String provider, int status, Bundle extras) {
+			// TODO Auto-generated method stub
+			
+		}
+	}
+	public void getDistancenTime(String url) {
+		// TODO Auto-generated method stub
+		String result = "";
+		InputStream is = null;
+    	try
+    	{
+    		HttpClient httpclient = new DefaultHttpClient();
+    		HttpPost httppost = new HttpPost(url);
+    		HttpResponse response = httpclient.execute(httppost);
+    		HttpEntity entity = response.getEntity();
+    		is = entity.getContent();
+    	} 
+    	catch(Exception e) 
+    	{
+    		Log.e("log_tag", "Error in http conection"+e.toString());
+    	}
+    	try 
+    	{
+    		BufferedReader reader = new BufferedReader(
+    		new InputStreamReader(is,"iso-8859-1"),8);
+    		StringBuilder sb = new StringBuilder();
+    		String line = null;
+    		while ( (line = reader.readLine() ) != null) {
+    		sb.append(line + "\n"); }
+    		is.close();
+    		result=sb.toString();
+
+    	}
+    	catch(Exception e)
+    	{
+    		Log.e("log_tag", "Error converting result "+e.toString());
+    	}
+		try 
+		{
+			JSONObject rootObj = new JSONObject(result); //rootObj ist jetzt ein dict
+			JSONArray routes = (JSONArray) rootObj.get("routes");
+			if(routes.length()<1)
+				distance.setText("ERROR no route there");
+			JSONObject firstRoute = routes.getJSONObject(0);
+			JSONArray legs = (JSONArray) firstRoute.get("legs");
+			if(legs.length()<1)
+				distance.setText("ERROR no legs there");
+			JSONObject firstLeg = legs.getJSONObject(0);
+			JSONObject durationObject = (JSONObject) firstLeg.get("duration");
+			JSONObject distanceObject = (JSONObject) firstLeg.get("distance");
+			JSONObject startaddObject = (JSONObject) firstLeg.get("start_location");
+			JSONObject endaddObject = (JSONObject) firstLeg.get("end_location");
+			String startadd = (String)firstLeg.get("start_address");
+			String endadd = (String)firstLeg.get("end_address");
+
+			timeToDestination = (String) durationObject.get("text");
+			distanceToDestination = (String) distanceObject.get("text");
+			fromLat = (Double) startaddObject.getDouble("lat");
+			fromLon = (Double) startaddObject.getDouble("lng");
+			
+			toLat = (Double) endaddObject.getDouble("lat");
+			toLon = (Double) endaddObject.getDouble("lng");
+			distance.setText("Time: "+timeToDestination+" "+"and Distance: "+distanceToDestination);
+			
+			System.out.println("start_Location: "+fromLat+","+fromLon+"\nend_location"+toLat+","+toLon);
+			System.out.println("start_address: "+startadd+"\nend_address: "+endadd);
+			
+		} 
+		catch (JSONException e) 
+		{
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	public static String getUrl(String fromAdress, String toAdress) // connect to map web service
+    {
+	    StringBuffer urlString = new StringBuffer();
+	    urlString.append("http://maps.google.com/maps/api/directions/json?origin=");
+	    urlString.append(fromAdress.toString());
+	    urlString.append("&destination=");
+	    urlString.append(toAdress.toString());
+	    urlString.append("&sensor=false");
+	    return urlString.toString();
+    }
 }
